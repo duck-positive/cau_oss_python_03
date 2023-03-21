d9c9c06f289215b5e9a079632dcca5e74dc00d6c
Gyeongdeok Seo <gyeondeo@gmail.com>
2023-03-21 15:03:42 +0900

    Update volume_calc.py
    총 길이에 따른 요금 계산 추가

--- volume_calc.py
+++ volume_calc.py
@@ -3,3 +3,19 @@ width = float(input("세로:"))
 height = float(input("높이:"))
 volume = width*height*length
 print("박스의 부피는 ",volume,"입니다.")
+
+total_length = width+height+length
+
+if total_length <= 80:
+    price = 5000
+elif 80 < total_length and total_length <= 100:
+    price = 8000
+elif 100 < total_length and total_length <= 120:
+    price = 10000
+elif 120 < total_length and total_length <= 160:
+    price = 13000
+else:
+    price = "배송 불가"
+ 
+print("총 길이: ",total_length)
+print("택배 요금: ", price)
